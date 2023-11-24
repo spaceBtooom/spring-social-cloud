43b69e04c50f615de837d6101cf245880da087f8
SaboEvgeny <evgenysabo@gmail.com>
2023-11-24 13:43:39 +0300

    added home controller

---
+++ src/main/java/com/springapp/springsocialcloud/SpringSocialCloudApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.springsocialcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapp/springsocialcloud/SpringSocialCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.springsocialcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
