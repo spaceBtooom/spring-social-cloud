691cee57d5d825789c365be8ccc02a699195cff4
SaboEvgeny <evgenysabo@gmail.com>
2023-11-26 00:13:04 +0300

    added UsernamePasswordTokenAuthentication

--- src/test/java/com/springapp/springsocialcloud/HomeControllerTest.java
+++ src/test/java/com/springapp/springsocialcloud/HomeControllerTest.java
@@ -3,6 +3,7 @@ package com.springapp.springsocialcloud;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -17,7 +18,8 @@ class HomeControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void shouldDisplayHomePage() throws Exception {
+    @WithMockUser
+    void authenticatedUserShouldDisplayHomePage() throws Exception {
         mockMvc
                 .perform(get("/"))
                 .andExpect(status().isOk());
